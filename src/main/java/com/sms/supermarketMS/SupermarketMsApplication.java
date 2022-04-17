cfeff57abf8890e38a33076a105af8bbf8fd2e18
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2022-04-17 03:08:58 +0100

    initial commit

---
+++ src/main/java/com/sms/supermarketMS/SupermarketMsApplication.java
@@ -0,0 +1,13 @@
+package com.sms.supermarketMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketMsApplication.class, args);
+	}
+
+}
